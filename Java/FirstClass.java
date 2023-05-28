6526e0770de19466e22f5cc928eda2a5b703859c
星落尘夕 <2594735042@qq.com>
2023-05-28 09:30:47 +0800

    第一个版本

---
+++ Java/FirstClass.java
@@ -0,0 +1,15 @@
+public class FirstClass {
+
+    public boolean isTrue(boolean b) {
+        if (b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
